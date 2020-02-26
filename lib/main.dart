9c4a7ac32206ef83cf3579c6678be5978d91f224
Ryan McGuire <ryanadmcguire@gmail.com>
2020-02-25 19:00:38 -0800

    add menu firebase functionality
    need to finish menu object implementation

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,90 +38,160 @@ class RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         title: Text('Wildcat Mobile Order Shell'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: () {}), // placeholder with no functionality
+          IconButton(icon: Icon(Icons.list), onPressed: () {}),
+          // placeholder with no functionality
         ],
       ),
-      body: _buildSuggestions(),
+      body: Builder(builder: (context) => _buildMenu(context)),
     );
-
-  }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      });
   }
 
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asLowerCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon( //Adds an icon trailing at the end of each row
-        alreadySaved ? Icons.favorite /*heart icon*/ : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          }
-          else {
-            _saved.add(pair);
-          }
-        });
+//  Widget _buildSuggestions() {
+//    return ListView.builder(
+//      padding: const EdgeInsets.all(16.0),
+//      itemBuilder: /*1*/ (context, i) {
+//        if (i.isOdd) return Divider(); /*2*/
+//
+//        final index = i ~/ 2; /*3*/
+//        if (index >= _suggestions.length) {
+//          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+//        }
+//        return _buildRow(_suggestions[index]);
+//      });
+//  }
+
+  Widget _buildMenu(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('menus').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        return _buildList(context, snapshot.data.documents);
       },
     );
   }
 
-  // navigates to another page
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text( 
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-          .divideTiles(
-            context: context,
-            tiles: tiles,
-          )
-          .toList();
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    int idx =
+        snapshot.indexWhere((snap) => snap.documentID == 'Coffee Place 1');
+    print(idx);
 
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final item = MenuItem.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(item.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(item.name),
+          trailing: Text(item.price.toString()),
+          onTap: () => print(item),
+        ),
       ),
     );
   }
 }
 
+//  Widget _buildRow(WordPair pair) {
+//    final bool alreadySaved = _saved.contains(pair);
+//    return ListTile(
+//      title: Text(
+//        pair.asLowerCase,
+//        style: _biggerFont,
+//      ),
+//      trailing: Icon( //Adds an icon trailing at the end of each row
+//        alreadySaved ? Icons.favorite /*heart icon*/ : Icons.favorite_border,
+//        color: alreadySaved ? Colors.red : null,
+//      ),
+//      onTap: () {
+//        setState(() {
+//          if (alreadySaved) {
+//            _saved.remove(pair);
+//          }
+//          else {
+//            _saved.add(pair);
+//          }
+//        });
+//      },
+//    );
+//  }
+
+//  // navigates to another page
+//  void _pushSaved() {
+//    Navigator.of(context).push(
+//      MaterialPageRoute<void>(
+//        builder: (BuildContext context) {
+//          final Iterable<ListTile> tiles = _saved.map(
+//            (WordPair pair) {
+//              return ListTile(
+//                title: Text(
+//                  pair.asPascalCase,
+//                  style: _biggerFont,
+//                ),
+//              );
+//            },
+//          );
+//          final List<Widget> divided = ListTile
+//          .divideTiles(
+//            context: context,
+//            tiles: tiles,
+//          )
+//          .toList();
+//
+//          return Scaffold(
+//            appBar: AppBar(
+//              title: Text('Saved Suggestions'),
+//            ),
+//            body: ListView(children: divided),
+//          );
+//        },
+//      ),
+//    );
+//  }
+//}
+
 class RandomWords extends StatefulWidget {
-  @override 
+  @override
   RandomWordsState createState() => RandomWordsState();
 }
 
-/*
-  Creating an infinite scrolling ListView
-*/
+// TODO: Add Category to MenuItem
+class MenuItem {
+  final String name;
+  final double price;
+  final String location;
+  final DocumentReference reference;
+
+  MenuItem.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['price'] != null),
+        assert(map['location'] != null),
+        location = map['location'],
+        name = map['name'],
+        price = map['price'];
+
+  MenuItem.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+}
+
+// TODO: Fix Menu constructor
+class Menu {
+  final String location;
+  List<MenuItem> items;
+  DocumentReference reference;
+
+  Menu.fromList(List<dynamic> list, {this.reference})
+      : location = 'Coffee_place1',
+        items = list;
+}
