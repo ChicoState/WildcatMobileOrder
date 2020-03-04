b057eaaee6414ba3ecfa71ebfb3b15466b83f0ae
Gage Smith <gage.smith.andrew@gmail.com>
2020-03-04 11:05:39 -0800

    Stashed changes for login.dart stuff in order to integrate it with the working master branch code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:WildcatMobileOrder/menu.dart';
 
-import 'login.dart';
 void main() => runApp(MyApp());
 
+final ThemeData td = ThemeData(
+  primaryColor: Colors.red[800],
+);
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Wildcat Mobile Order Shell',
+        theme: td,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeScreen(),
+          '/locations': (context) => LocationSelection(),
+          '/menu': (context) => MenuView(),
+        });
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//* All above area is for merge with master branch
-// class MyApp extends StatelessWidget {
-//   final landingTitle = 'Wildcat Mobile Order';
-//   @override
-//   Widget build(BuildContext context) {
-//     //this pulls from the package of english_words and creates a random wordPair
-//     return MaterialApp(
-      
-//       title: 'Wildcat Mobile Order Shell',
-//       theme: ThemeData(
-//         primaryColor: Color(0xff960000),
-//       ),
-//       home: LandingPage(title: landingTitle),
-//     );
-//   }
-// }
-
-// class LandingPage extends StatelessWidget {
-//   final String title;
-
-//   LandingPage({Key key, this.title}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(title),
-//       ),
-//       body: Center(
-//         child: RaisedButton(
-//           child: Text('Login'),
-//           onPressed: () {
-//             //Navigator.pop(context)
-//             Navigator.push(
-//               context,
-//               MaterialPageRoute(builder: (context) => LoginPage()),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            onPressed: () => Navigator.pushNamed(context, '/locations'),
+            child: Text('Select a location')),
+      ),
+    );
+  }
+}
