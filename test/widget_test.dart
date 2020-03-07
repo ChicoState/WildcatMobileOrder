9408f3bee7868d08535e4c34c07443561376309c
Gage Smith <gage.smith.andrew@gmail.com>
2020-03-07 13:08:37 -0800

    Updated the import package for widget tester to show the correct app name of 'WildcatMobileOrder'.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wildcat_MO/main.dart';
+import 'package:WildcatMobileOrder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
