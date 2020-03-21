deeb2b3353f67d1e7b12c0b5da4c1ab2e56e181f
Gage Smith <gage.smith.andrew@gmail.com>
2020-03-20 19:29:44 -0700

    Removed the option to sign-in with email and password as well as removed the registration.dart file. Registration is automatically done with the Google Authentication.

--- lib/screens/login/sign_in.dart
+++ lib/screens/login/sign_in.dart
@@ -1,15 +1,8 @@
 import 'package:WildcatMobileOrder/services/auth.dart';
-
-// import 'package:WildcatMobileOrder/shared/constants.dart';
 import 'package:WildcatMobileOrder/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  final Function toggleView;
-
-  //Constructor for SignIn widget instantiating the toggleView value
-  SignIn({this.toggleView});
-
   @override
   _SignInState createState() => _SignInState();
 }
@@ -23,8 +16,6 @@ class _SignInState extends State<SignIn> {
   bool loading = false;
 
   //text field state
-  String email = '';
-  String password = '';
   String error = '';
 
   @override
@@ -36,15 +27,8 @@ class _SignInState extends State<SignIn> {
             appBar: AppBar(
               backgroundColor: Colors.red[900],
               elevation: 0.0,
-              title: Text('Sign in to Wildcat MO'),
+              title: Text('Sign/Register in to Wildcat MO'),
               actions: <Widget>[
-                FlatButton.icon(
-                  icon: Icon(Icons.person),
-                  label: Text('Register'),
-                  onPressed: () {
-                    widget.toggleView();
-                  },
-                ),
               ],
             ),
             body: Container(
@@ -56,53 +40,6 @@ class _SignInState extends State<SignIn> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     mainAxisSize: MainAxisSize.max,
                     children: <Widget>[
-                      //* Username Field
-                      // SizedBox(height: 20.0),
-                      // TextFormField(
-                      //   //Modifying the constant textInputDecoration -> constants.dart hintText
-                      //   decoration:
-                      //       textInputDecoration.copyWith(hintText: 'Email'),
-                      //   validator: (val) =>
-                      //       val.isEmpty ? 'Enter a valid email' : null,
-                      //   onChanged: (val) {
-                      //     setState(() => email = val);
-                      //   },
-                      // ),
-                      //* Password Field
-                      // SizedBox(height: 20.0),
-                      // TextFormField(
-                      //     decoration:
-                      //         textInputDecoration.copyWith(hintText: 'Password'),
-                      //     validator: (val) => val.length < 6
-                      //         ? 'Password must be at least 6 characters'
-                      //         : null,
-                      //     obscureText:
-                      //         true, //* Fixes issue of obscuring the password while typing in field
-                      //     onChanged: (val) {
-                      //       setState(() => password = val);
-                      //     }),
-                      // SizedBox(height: 20.0),
-                      // RaisedButton(
-                      //     color: Colors.red[600],
-                      //     child: Text(
-                      //       'Sign In',
-                      //       style: TextStyle(color: Colors.white),
-                      //     ),
-                      //     onPressed: () async {
-                      //       if (_formKey.currentState.validate()) {
-                      //         //Set the loading state
-                      //         setState(() => loading = true);
-                      //         //This waits for the result of the sign in whether it is null or a user
-                      //         dynamic result = await _auth
-                      //             .signInWithEmailAndPassword(email, password);
-                      //         if (result == null) {
-                      //           setState(() {
-                      //             error = 'Invalid credentials';
-                      //             loading = false;
-                      //           });
-                      //         }
-                      //       }
-                      //     }),
                       SizedBox(height: 50),
                       Image(
                         image: AssetImage("graphics/WildcatMO.png"),
@@ -136,7 +73,7 @@ class _SignInState extends State<SignIn> {
                                   height: 35.0),
                               Padding(
                                 padding: const EdgeInsets.only(left: 10),
-                                child: Text('Sign in with Google',
+                                child: Text('Sign in with CSU Chico Gmail',
                                     style: TextStyle(
                                       fontSize: 20,
                                       color: Colors.white,
@@ -157,6 +94,7 @@ class _SignInState extends State<SignIn> {
                   ),
                 ),
               ),
+              //! Leaving in this portion in case there are issues with the upper portion when debugging
               //? Raised button for Anonymous Sign In option
               // child: RaisedButton(
               //   child: Text('Sign in Anon'),
