c98fda607413cd1f3ea3a7cabffe2b6b76e5bd4f
Nicolas Espino <nicolasespino@Nicolass-MBP.hsd1.ca.comcast.net>
2020-04-23 15:47:21 -0700

    Hours  show if location is OPEN or CLOSED

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -12,17 +12,47 @@ class Locations {
 
 /// Represents a single location
 class LocationItem {
-  final String openTime;
-  final String closeTime;
+  int openTime;
+  int openMin;
+  int closeTime;
+  int closeMin;
   final String name;
 
   LocationItem.fromMap(Map<String, dynamic> map)
       : openTime = map['opentime'],
+        openMin = map['openMin'],
         closeTime = map['closetime'],
+        closeMin = map['closeMin'],
         name = map['name'];
 
-  String getOpenHours() {
-    return 'Opens at $openTime and closes at $closeTime';
+  String getOpenHours() { 
+    String frmtSpcng = "               ";
+    String printOpen = "$openTime:$openMin AM";
+    String printClose;
+  
+    if (closeTime > 12) {
+      closeTime = closeTime - 12;
+      printClose = "$closeTime:$closeMin PM";
+    }
+    else {
+      printClose = "$closeTime:$closeMin PM";
+    } 
+    return '  Hours: \n $frmtSpcng $printOpen - $printClose';
+  }
+
+  String isOpen () {
+    var curDate = new DateTime.now();
+    int curHr = curDate.hour;
+    int curMin = curDate.minute;
+    String res = "OPEN";
+    if ( (curHr > closeTime) && (curMin > closeMin) ) {
+      res = "CLOSED";
+    }
+
+    if ( (curHr < openTime) && (curMin < openMin) ) {
+      res = "CLOSED";
+    }
+    return ' is $res';
   }
 
   String getOrderString() {
