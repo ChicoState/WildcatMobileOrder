7be07cadf6daff37fd8e2800f35b5c7e76a4df1e
ramcguire <ryanadmcguire@gmail.com>
2020-05-05 14:38:57 -0700

    update order confirmation

--- lib/screens/order_confirmation.dart
+++ lib/screens/order_confirmation.dart
@@ -7,21 +7,55 @@ class OrderConfirmation extends StatelessWidget {
 
   OrderConfirmation(this.order);
 
+  String orderInformation(DateTime date, String orderId)  {
+    return '${date.toString()}\n$orderId';
+  }
+
+  Widget _buildOrder(BuildContext context, DocumentSnapshot snapshot) {
+    int itemCount = 0;
+    snapshot.data['items'].forEach((item) {
+      itemCount += item.qty;
+    });
+    DateTime orderTime = snapshot.data['ordertime'].toDate();
+    return Column(
+      children: <Widget>[
+        Flexible(
+          flex: 5,
+          child: ListView.builder(
+              itemCount: itemCount,
+              itemBuilder: (context, idx) {
+                return Text(snapshot.data['items'][idx]);
+              }
+          ),
+        ),
+        Flexible(
+          flex: 4,
+          child: ListTile(
+            isThreeLine: true,
+            title: Text('Order from ${snapshot.data['location']}'),
+            subtitle: Text(orderInformation(orderTime, snapshot.data['orderid'])),
+            trailing: Text('\$${snapshot.data['price']}'),
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Order Confirmation'),
-      ),
-      body: FutureBuilder(
-        future: ,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Container();
-          }
-          return Loading();
-        }
-      )
+        appBar: AppBar(
+          title: Text('Order Confirmation'),
+        ),
+        body: FutureBuilder(
+            future:,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Container();
+              }
+              return Loading();
+            }
+        )
     );
   }
 }
