40f976bbf1f019b34345754c623c7b1bd8aac6ba
Nicolas Espino <nicolasespino@Nicolass-MBP.hsd1.ca.comcast.net>
2020-05-07 14:19:26 -0700

    updated Testing LandingWidget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,17 +14,10 @@ void main() {
   // Define a test. The TestWidgets function also provides a WidgetTester
   // to work with. The WidgetTester allows building and interacting
   // with widgets in the test environment.
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(Landing('naespino@mail.csuchico.edu'));
+  testWidgets('MyWidget has a user: ', (WidgetTester tester) async {
 
-    // Create the Finders.
-    final titleFinder = find.text('naespino@mail.csuchico.edu');
-    // final messageFinder = find.text('M');
+    await tester.pumpWidget(Landing("naespino@mail.csuchico.edu"));
 
-    // Use the `findsOneWidget` matcher provided by flutter_test to
-    // verify that the Text widgets appear exactly once in the widget tree.
-    expect(titleFinder, findsOneWidget);
-    // expect(messageFinder, findsOneWidget);
+   expect(find.text('_showLocations'), findsWidgets);
   });
 }
