73c5dccc1242df173ded6b561d8cb8388d93b115
Nicolas Espino <nicolasespino@Nicolass-MBP.hsd1.ca.comcast.net>
2020-05-07 14:40:03 -0700

    updated Testing LandingWidget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,13 +11,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:WildcatMobileOrder/main.dart';
 void main() {
+final UserRepository userRepository = UserRepository();
   // Define a test. The TestWidgets function also provides a WidgetTester
   // to work with. The WidgetTester allows building and interacting
   // with widgets in the test environment.
-  testWidgets('MyWidget has a user: ', (WidgetTester tester) async {
-
-    await tester.pumpWidget(Landing("naespino@mail.csuchico.edu"));
-
-   expect(find.text('_showLocations'), findsWidgets);
-  });
+testWidgets('smoke test', (WidgetTester tester) async {
+  final app = MyApp(userRepository);
+  await tester.pumpWidget(app);
+  expect(find.text("0"), findsOneWidget);
+  await tester.tap(find.byIcon(Icons.add));
+  await tester.pump();
+  expect(find.text("1"), findsOneWidget);
+});
 }
