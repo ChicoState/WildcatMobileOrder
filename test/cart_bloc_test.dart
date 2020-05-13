36a512074462fc52379b7fca579e39258fb6b7c7
Nicolas Espino <nicolasespino@Nicolass-MBP.hsd1.ca.comcast.net>
2020-05-13 12:37:55 -0700

    Testing CartBloc

---
+++ test/cart_bloc_test.dart
@@ -0,0 +1,24 @@
+import 'package:WildcatMobileOrder/blocs/cart_bloc/bloc.dart';
+import 'package:WildcatMobileOrder/repositories/cart_repository/cart_repository.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockCartBloc extends MockBloc<CartEvent,CartState> implements CartBloc {}
+
+void main () {
+  group('CartBlocTesting', () {
+    blocTest(
+      'emits [] when nothing is added',
+      build: () async => CartBloc(),
+      expect: [],
+    );
+
+    blocTest(
+      'emits [1] when CounterEvent.increment is added',
+      build: () async => CartBloc(),
+      act: (bloc) => bloc.add(CartEvent),
+      expect: [1],
+    );
+  });
+}
