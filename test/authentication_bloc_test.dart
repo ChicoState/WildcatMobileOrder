06c86c7bb1ba89a841bf73f5a96a28def2b1a2c4
Gage Smith <gage.smith.andrew@gmail.com>
2020-05-14 16:43:51 -0700

    Retreived a firebase auth user from google sign in to run tests on.

--- test/authentication_bloc_test.dart
+++ test/authentication_bloc_test.dart
@@ -1,33 +1,80 @@
 import 'package:WildcatMobileOrder/blocs/blocs.dart';
 import 'package:WildcatMobileOrder/repositories/repositories.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mockito/mockito.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MockUserRepository extends Mock implements UserRepository {}
 
 void main() {
   AuthenticationBloc authenticationBloc;
   //Using mockito to create a mock user repo object to work with
-  MockUserRepository userRepository;
+  UserRepository userRepository;
 
   setUp(() {
-    userRepository = MockUserRepository();
+    userRepository = UserRepository(
+        firebaseAuth: MockFirebaseAuth(), googleSignin: MockGoogleSignIn());
     authenticationBloc = AuthenticationBloc(userRepository: userRepository);
+  });
+
+  test("testing async", () async {
+    final user = await userRepository.signInWithGoogle();
+    print(user);
   });
 
   tearDown(() {
     authenticationBloc?.close();
   });
 
-  test('initial state is correct', () {
+  test('testing an initial state', () {
     expect(authenticationBloc.initialState, Uninitialized());
   });
 
-  test('close does not emit new states', () {
-    expectLater(
-      authenticationBloc,
-      emitsInOrder([Uninitialized(), emitsDone]),
-    );
-    authenticationBloc.close();
+  // test('close does not emit new states', () {
+  //   expectLater(
+  //     authenticationBloc,
+  //     emitsInOrder([Uninitialized(), emitsDone]),
+  //   );
+  //   authenticationBloc.close();
+  // });
+
+  group('AppStart', () {
+    test('emits [uninitialized, unauthenticated] for invalid token', () {
+      final expectResponse = [
+        Uninitialized(),
+        Unauthenticated(),
+      ];
+
+      when(userRepository.isSignedIn()).thenAnswer((_) => Future.value(false));
+
+      expectLater(
+        authenticationBloc,
+        emitsInOrder(expectResponse),
+      );
+
+      authenticationBloc.add(AppStarted());
+    });
+  });
+
+  group('LoggedIn', () {
+    test('emits [uninitialized, authenticated] when login successful',
+        () async {
+      final user = await userRepository.signInWithGoogle();
+      final expectResponse = [
+        Uninitialized(),
+        Authenticated(user),
+      ];
+
+      expectLater(
+        authenticationBloc,
+        emitsInOrder(expectResponse),
+      );
+
+      authenticationBloc.add(LoggedIn());
+    });
   });
 }
