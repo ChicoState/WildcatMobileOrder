aa82a819ac0a5a566743e3d23b99a22185b787a6
Gage Smith <gage.smith.andrew@gmail.com>
2020-05-14 20:10:30 -0700

    Finally got a basic widget build to run. Bloc is getting in the way for some portions of it.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,20 +1,40 @@
 import 'package:WildcatMobileOrder/repositories/repositories.dart';
+import 'package:WildcatMobileOrder/screens/screens.dart';
+import 'package:WildcatMobileOrder/blocs/blocs.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:WildcatMobileOrder/screens/login.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
+import 'package:mockito/mockito.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  UserRepository userRepository;
+  AuthenticationBloc authenticationBloc;
 
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows building and interacting
-  // with widgets in the test environment.
-//  testWidgets('smoke test', (WidgetTester tester) async {
-//    final app = MyApp(userRepository, menuRepository);
-//    await tester.pumpWidget(app);
-//    expect(find.text("0"), findsOneWidget);
-//    await tester.tap(find.byIcon(Icons.add));
-//    await tester.pump();
-//    expect(find.text("1"), findsOneWidget);
-//  });
+  setUp(() {
+    userRepository = UserRepository(
+        firebaseAuth: MockFirebaseAuth(), googleSignIn: MockGoogleSignIn());
+    authenticationBloc = AuthenticationBloc(userRepository: userRepository);
+  });
 
+  group('login screen init', () {
+    testWidgets('Main UI Test', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+        backgroundColor: Colors.grey[800],
+        appBar: AppBar(
+          title: Center(
+            child: Text('Wildcat Mobile Order'),
+          ),
+        ),
+        body: Container(),
+      )));
 
+      final titleFinder = find.text('Wildcat Mobile Order');
+      expect(titleFinder, findsOneWidget);
+    });
+  });
 }
